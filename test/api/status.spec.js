af3702a4ba24057ebdac68fdabdd13794e14a66d
Perry Mitchell <perry@perrymitchell.net>
2018-09-26 15:23:37 +0300

    Add more status tests

--- test/api/status.spec.js
+++ test/api/status.spec.js
@@ -5,14 +5,17 @@ const pkgInfo = require("../../package.json");
 const { createApp } = require("../express.js");
 
 describe("/status", function() {
+    beforeEach(function() {
+        this.service = new Service();
+    });
+
+    afterEach(function() {
+        this.service.shutdown();
+    });
+
     describe("when service is ready", function() {
         beforeEach(function() {
-            this.service = new Service();
             return this.service.initialise();
-        });
-
-        afterEach(function() {
-            this.service.shutdown();
         });
 
         it("responds with 'ok' status", function() {
@@ -23,6 +26,46 @@ describe("/status", function() {
                 .expect(200)
                 .then(response => {
                     expect(response.body).to.have.property("status", "ok");
+                    expect(response.body).to.have.property("started")
+                        .that.is.a("number")
+                        .and.is.above(earlierTime);
+                    expect(response.body).to.have.property("api", pkgInfo.version);
+                });
+        });
+    });
+
+    describe("when service has been shut down", function() {
+        beforeEach(function() {
+            return this.service.initialise().then(() => {
+                this.service.shutdown();
+            });
+        });
+
+        it("responds with 'offline' status", function() {
+            const earlierTime = Date.now() - ms("2m");
+            return request(createApp(this.service))
+                .get("/status")
+                .set("Accept", "application/json")
+                .expect(200)
+                .then(response => {
+                    expect(response.body).to.have.property("status", "offline");
+                    expect(response.body).to.have.property("started")
+                        .that.is.a("number")
+                        .and.is.above(earlierTime);
+                    expect(response.body).to.have.property("api", pkgInfo.version);
+                });
+        });
+    });
+
+    describe("when service has not been initialised", function() {
+        it("responds with 'init' status", function() {
+            const earlierTime = Date.now() - ms("2m");
+            return request(createApp(this.service))
+                .get("/status")
+                .set("Accept", "application/json")
+                .expect(200)
+                .then(response => {
+                    expect(response.body).to.have.property("status", "init");
                     expect(response.body).to.have.property("started")
                         .that.is.a("number")
                         .and.is.above(earlierTime);
