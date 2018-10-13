8018f26a0a0d85999464c64eba8eb6f14be73740
Perry Mitchell <perry@kiosked.com>
2018-10-13 14:48:33 +0300

    Add GET block to status tests

--- test/api/status.spec.js
+++ test/api/status.spec.js
@@ -13,67 +13,69 @@ describe("/status", function() {
         this.service.shutdown();
     });
 
-    describe("when service is ready", function() {
-        beforeEach(function() {
-            return this.service.initialise();
-        });
+    describe("GET", function() {
+        describe("when service is ready", function() {
+            beforeEach(function() {
+                return this.service.initialise();
+            });
 
-        it("responds with 'ok' status", function() {
-            const earlierTime = Date.now() - ms("2m");
-            return request(createApp(this.service))
-                .get("/status")
-                .set("Accept", "application/json")
-                .expect(200)
-                .then(response => {
-                    expect(response.body).to.have.property("status", "ok");
-                    expect(response.body)
-                        .to.have.property("started")
-                        .that.is.a("number")
-                        .and.is.above(earlierTime);
-                    expect(response.body).to.have.property("api", pkgInfo.version);
-                });
-        });
-    });
-
-    describe("when service has been shut down", function() {
-        beforeEach(function() {
-            return this.service.initialise().then(() => {
-                this.service.shutdown();
+            it("responds with 'ok' status", function() {
+                const earlierTime = Date.now() - ms("2m");
+                return request(createApp(this.service))
+                    .get("/status")
+                    .set("Accept", "application/json")
+                    .expect(200)
+                    .then(response => {
+                        expect(response.body).to.have.property("status", "ok");
+                        expect(response.body)
+                            .to.have.property("started")
+                            .that.is.a("number")
+                            .and.is.above(earlierTime);
+                        expect(response.body).to.have.property("api", pkgInfo.version);
+                    });
             });
         });
 
-        it("responds with 'offline' status", function() {
-            const earlierTime = Date.now() - ms("2m");
-            return request(createApp(this.service))
-                .get("/status")
-                .set("Accept", "application/json")
-                .expect(200)
-                .then(response => {
-                    expect(response.body).to.have.property("status", "offline");
-                    expect(response.body)
-                        .to.have.property("started")
-                        .that.is.a("number")
-                        .and.is.above(earlierTime);
-                    expect(response.body).to.have.property("api", pkgInfo.version);
+        describe("when service has been shut down", function() {
+            beforeEach(function() {
+                return this.service.initialise().then(() => {
+                    this.service.shutdown();
                 });
-        });
-    });
+            });
 
-    describe("when service has not been initialised", function() {
-        it("responds with 'init' status", function() {
-            const earlierTime = Date.now() - ms("2m");
-            return request(createApp(this.service))
-                .get("/status")
-                .set("Accept", "application/json")
-                .expect(200)
-                .then(response => {
-                    expect(response.body).to.have.property("status", "init");
-                    expect(response.body)
-                        .to.have.property("started")
-                        .that.is.a("number")
-                        .and.is.above(earlierTime);
-                    expect(response.body).to.have.property("api", pkgInfo.version);
-                });
+            it("responds with 'offline' status", function() {
+                const earlierTime = Date.now() - ms("2m");
+                return request(createApp(this.service))
+                    .get("/status")
+                    .set("Accept", "application/json")
+                    .expect(200)
+                    .then(response => {
+                        expect(response.body).to.have.property("status", "offline");
+                        expect(response.body)
+                            .to.have.property("started")
+                            .that.is.a("number")
+                            .and.is.above(earlierTime);
+                        expect(response.body).to.have.property("api", pkgInfo.version);
+                    });
+            });
+        });
+
+        describe("when service has not been initialised", function() {
+            it("responds with 'init' status", function() {
+                const earlierTime = Date.now() - ms("2m");
+                return request(createApp(this.service))
+                    .get("/status")
+                    .set("Accept", "application/json")
+                    .expect(200)
+                    .then(response => {
+                        expect(response.body).to.have.property("status", "init");
+                        expect(response.body)
+                            .to.have.property("started")
+                            .that.is.a("number")
+                            .and.is.above(earlierTime);
+                        expect(response.body).to.have.property("api", pkgInfo.version);
+                    });
+            });
         });
     });
 });
