455121b24f0f4f4c5a6627c817a0d6bda403a4ad
Perry Mitchell <perry@kiosked.com>
2018-10-13 15:00:46 +0300

    Add query options to the API

--- source/api/query.js
+++ source/api/query.js
@@ -7,16 +7,24 @@ function handleJobsQuery(req, res) {
     const log = getLog(req);
     return Promise.resolve()
         .then(() => {
-            const { query } = req.body;
+            const { query, options = {} } = req.body;
             if (!query || typeof query !== "object") {
                 throw new VError(
                     { info: { code: "ERR_JOB_QUERY_INVALID" } },
                     "Invalid jobs query object"
                 );
+            } else if (options && typeof options !== "object") {
+                throw new VError(
+                    { info: { code: "ERR_JOB_QUERY_OPTS_INVALID" } },
+                    "Invalid jobs query options object"
+                );
             }
-            log.info({ query }, "Performing jobs query");
+            if (options.limit === -1) {
+                options.limit = Infinity;
+            }
+            log.info({ query, options }, "Performing jobs query");
             const finalQuery = mapSmartQueryParameters(query);
-            return service.queryJobs(finalQuery);
+            return service.queryJobs(finalQuery, options);
         })
         .then(jobs => {
             res.send(
